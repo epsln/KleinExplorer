9397694f60846c83bf46544d58b6c81e0dbe55c1
epsilon <martin.olivier1997@gmail.com>
2024-11-04 11:15:03 +0100

    fixed exploration logic

--- src/tree_explorer.cpp
+++ src/tree_explorer.cpp
@@ -1,6 +1,7 @@
 #include <complex>
 #include <cstring>
 #include <cmath>
+#include "ofMain.h"
 
 #include "tree_explorer.h"
 #include "Mobius.h"
@@ -51,6 +52,7 @@ int KleinExplorer::branch_terminated(){
 	complex<float> comp_fp;
 
 	float o = 0;
+	ofLog(OF_LOG_NOTICE, "Branch termination @ level: %d", level) ;
 	for (int i = 0; i < fixedPoints[idx_gen].size() - 1; i++){
 		fp = fixedPoints[idx_gen][i];
 		comp_fp = fixedPoints[idx_gen][i + 1];
@@ -58,6 +60,8 @@ int KleinExplorer::branch_terminated(){
 		p = words[level].apply(fp);
 		comp_p = words[level].apply(comp_fp);
 		o += abs(p - comp_p);
+		ofLog(OF_LOG_NOTICE, "p0: %f %f, p1: %f %f, abs: %f", real(p), imag(p), real(comp_p), imag(comp_p), abs(p - comp_p)); 
+
 		if (abs(p - comp_p) > epsilon)
 			return 0;
 	}
@@ -69,13 +73,21 @@ int KleinExplorer::branch_terminated(){
 		comp_p = words[level].apply(comp_fp);
 
 		//TODO: Implement me (using of methods)
-		//line(p, comp_p, img);
+		float x0 = ofMap(real(p), -1, 1, 0, 1080);
+		float y0 = ofMap(imag(p), -1, 1, 0, 1080);
+		float x1 = ofMap(real(comp_p), -1, 1, 0, 1080);
+		float y1 = ofMap(imag(comp_p), -1, 1, 0, 1080);
+		ofSetColor(225);
+		ofSetLineWidth(1);
+		ofLog(OF_LOG_NOTICE, "Point %d %d", x0, y0); 
+		ofDrawLine(x0, y0, x1, y1);
 	}
 
 	return 1;
 }
 
 void KleinExplorer::backward_move(){
+	ofLog(OF_LOG_NOTICE, "Backward move @ level: %d", level) ;
 	level -= 1;
 }
 
@@ -91,6 +103,7 @@ int KleinExplorer::available_turn(){
 }
 
 void KleinExplorer::turn_forward_move(){
+	ofLog(OF_LOG_NOTICE, "Turn Forward move @ level: %d", level) ;
 	int idx_gen = get_left_gen();
 
 	set_next_state(idx_gen);
@@ -105,12 +118,14 @@ void KleinExplorer::turn_forward_move(){
 }
 
 void KleinExplorer::forward_move(){
+	ofLog(OF_LOG_NOTICE, "Forward move @ level: %d", level) ;
 	int idx_gen = get_right_gen();
 
 	set_next_state(idx_gen);
 
 	tag[level + 1] = idx_gen;
 
+	words[level].print();
 	if (level == -1)
 		words[0] = generators[idx_gen];
 	else
@@ -119,25 +134,24 @@ void KleinExplorer::forward_move(){
 }
 
 void KleinExplorer::compute(){
-	tag[0] = start_tag;
+	tag[0] = 0;
 	words[0] = generators[0];
-	state[0] = start_state;
+	state[0] = 1;
 
+	ofLog(OF_LOG_NOTICE, "Compute @ level %d", level); 
 	while (!(level == -1 && tag[0] == start_tag)){
-		while (branch_terminated()){
-
+		while (branch_terminated())
 			forward_move();
 
-			do	
-				backward_move();
-			while(available_turn() == 1 or level == -1);
+		do	
+			backward_move();
+		while(available_turn() != 1 || level != -1);
 
-			if (level == -1 and tag[0] == start_tag)
-				break;
+		if (level == -1 && tag[0] == start_tag)
+			break;
 
-			turn_forward_move();
+		turn_forward_move();
 
-		}
 	}
 }
 
@@ -146,9 +160,12 @@ KleinExplorer::KleinExplorer(int max_d, float eps, KleinFractalModel kfm){
 	epsilon = eps; 
 	memcpy(generators, kfm.generators, sizeof(MobiusT) * 4);
 	fixedPoints = kfm.fixedPoints;
-  fill(tag.begin(), tag.end(), 0);//Probably will screw things up
-  fill(state.begin(), state.end(), 0);
+	tag.reserve(max_d);
+	state.reserve(max_d);
+	words.reserve(max_d);
+	fill(tag.begin(), tag.end(), 0);//Probably will screw things up
+	fill(state.begin(), state.end(), 0);
 
 	MobiusT defaultWord = MobiusT(0,0,0,0);
-  fill(words.begin(), words.end(), defaultWord);
+	fill(words.begin(), words.end(), defaultWord);
 }
